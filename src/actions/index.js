d1ad2bcfe6a6c9238369882bbc00a9d0a79e7d2b
kai-haskell <me@kaihaskell.com>
2020-01-06 17:34:57 -0500

    Finished basic Farmer actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -51,3 +51,56 @@ export const uRegisterRequest = userData => dispatch => {
     .then(res => dispatch({ type: UREGISTER_SUCCESS, payload: res.data }))
     .catch(err => dispatch({ type: UREGISTER_FAILURE, payload: err }));
 };
+
+//FARMER SHOP ACTIONS
+export const FETCH_FOOD_START = "FETCH_FOOD_START";
+export const FETCH_FOOD_SUCCESS = "FETCH_FOOD_SUCCESS";
+export const FETCH_FOOD_FAILURE = "FETCH_FOOD_FAILURE";
+
+export const ADD_FOOD_START = "ADD_FOOD_START";
+export const ADD_FOOD_SUCCESS = "ADD_FOOD_SUCCESS";
+export const ADD_FOOD_FAILURE = "ADD_FOOD_FAILURE";
+
+export const UPDATE_FOOD_START = "UPDATE_FOOD_START";
+export const UPDATE_FOOD_SUCCESS = "UPDATE_FOOD_START";
+export const UPDATE_FOOD_FAILURE = "UPDATE_FOOD_FAILURE";
+
+export const DELETE_FOOD_START = "DELETE_FOOD_START";
+export const DELETE_FOOD_SUCCESS = "DELETE_FOOD_SUCCESS";
+export const DELETE_FOOD_FAILURE = "DELETE_FOOD_FAILURE";
+
+//GET
+export const fetchFood = () => dispatch => {
+  dipatch({ type: FETCH_FOOD_START });
+
+  AxiosWithAuth()
+    .get("/placeholder")
+    .then(res => dispatch({ type: FETCH_FOOD_SUCCESS, payload: res.data }))
+    .catch(err => dispatch({ type: FETCH_FOOD_FAILURE, payload: err }));
+};
+
+//POST
+export const addFood = newFood => dispatch => {
+  dispatch({ type: ADD_FOOD_START });
+  AxiosWithAuth("/placeholder")
+    .post(res => dispatch({ type: ADD_FOOD_SUCCESS, payload: res.data }))
+    .catch(err => dispatch({ type: ADD_FOOD_FAILURE, payload: err }));
+};
+
+//PUT
+export const addFood = (updatedFood, id) => dispatch => {
+  dispatch({ type: UPDATE_FOOD_START });
+  AxiosWithAuth(`/placeholder/${id}`, updatedFood)
+    .post(res => dispatch({ type: UPDATE_FOOD_SUCCESS, payload: res.data }))
+    .catch(err => dispatch({ type: UPDATE_FOOD_FAILURE, payload: err }));
+};
+
+//DELETE
+export const addFood = id => dispatch => {
+  dispatch({ type: DELETE_FOOD_START });
+  AxiosWithAuth(`/placeholder/${id}`)
+    .post(res => dispatch({ type: DELETE_FOOD_SUCCESS, payload: res.id }))
+    .catch(err => dispatch({ type: DELETE_FOOD_FAILURE, payload: err }));
+};
+
+//USER SHOPPING ACTIONS
