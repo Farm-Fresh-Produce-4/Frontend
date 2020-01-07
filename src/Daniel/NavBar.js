c4edc7b924c30fe429796b47a4f4a12842d5282c
jDanielDetes <jdanieldetes@gmail.com>
2020-01-07 12:00:24 -0500

    product card layout finished

---
+++ src/Daniel/NavBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export const NavBar = () => {
+  const navLinks = [
+    {
+        text: "Shop",
+        path: ""
+      },
+      {
+          text:"Farms" ,
+          path: ""
+      },
+      {
+          text:"Account" ,
+          path: ""
+      },
+      {
+          text:"Search" ,
+          path: ""
+      },
+      {
+          text:"Sign In" ,
+          path:""
+      }
+  ];
+
+  return <div></div>;
+};
