0c60309a6c34251445c1d7b69fcd54cb01764e7f
Eric Ferguson <turtle0888@gmail.com>
2020-01-06 18:03:09 -0600

    Add to files

--- src/components/FarmsList.js
+++ src/components/FarmsList.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axiosWithAuth from '../utils/axiosWithAuth';
 
 const FarmsList = () => {
+  // const [farms, setFarms] = useState('')
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get('/placeholder')
+      .then(res => {
+        setFarms(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <div>
       <p></p>
