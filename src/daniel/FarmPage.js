e7dccfd352d1eea1f3d7214cf999355dc74c56dc
jDanielDetes <jdanieldetes@gmail.com>
2020-01-08 20:09:17 -0500

    more changes to farmerpage

--- src/daniel/FarmPage.js
+++ src/daniel/FarmPage.js
@@ -11,6 +11,8 @@ export const FarmPage = () => {
             <div>
             <FNavBar/>
             <img class= 'Header' src={Header_image} alt=""/>
+            <h1>Fresh Produce Around The World!</h1>
+            <p class='headertext'>Choose any farms that firts your busy lifestyle. For your convenience, our farms offer many features that makes eating healthy much easier</p>
             </div>
             
             <FarmList/>
